c19973e137809d437d2578e0eaea99ab402633a9
DianaBeta <dianabetancourt7@gmail.com>
2018-12-06 18:25:55 +0100

    06.12.2018-added background on beginner activity and now starting to add the rhythm

--- app/src/main/java/com/example/diana/testrhythmer/beginnerActivity.java
+++ app/src/main/java/com/example/diana/testrhythmer/beginnerActivity.java
@@ -1,16 +1,18 @@
 package com.example.diana.testrhythmer;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class beginnerActivity extends AppCompatActivity {
-
+MediaPlayer beginnerSong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner);
+        beginnerSong=MediaPlayer.create(beginnerActivity.this,R.raw.beginner);
     }
 
     public void backtoBeginner(View view) {
@@ -30,4 +32,16 @@ public class beginnerActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HowToPlay2.class);
         startActivity(intent);
     }
+    public void play(View view) {
+        // play the song
+        beginnerSong.start();
+    }
+
+    protected void onPause(){
+        // pause the song
+        super.onPause();
+        beginnerSong.release();
+
+    }
+
 }
