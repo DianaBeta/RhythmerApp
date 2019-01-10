77ff817d63e6b93c402f74d82a29b73dbb4de335
isiltatlici <isiltatlicioglu@yahoo.com.tr>
2019-01-10 21:47:29 +0100

    - puase is disabled, due to complexity level (it will be hard for syching later on) - A new Method is defined, it is for Playing and Matching simultaneously. So the basic play feature is done through Play method (with its own submethods for onCompletion) and the other logical operations while playing the song are done in PlayANdMatch method. -Upon finishing the song, the user can a)replay the song b)Start the game - After starting the game the song starts and the green button appears - ready for user input - After it finishes user can a)replay the song b)Restart the game
    -A button Id is changed backed to user_button because it is used like that both in xml and java code. So if you change an
    Id on xml please make the change on java side as well (vice versa)
    
    -A String restart is added to String Resources

--- app/src/main/java/com/example/diana/testrhythmer/beginnerActivity.java
+++ app/src/main/java/com/example/diana/testrhythmer/beginnerActivity.java
@@ -18,12 +18,11 @@ public class beginnerActivity extends AppCompatActivity {
     ArrayList<Long> user_beat;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner);
-        beginnerSong = MediaPlayer.create(beginnerActivity.this, R.raw.beginner);
+        beginnerSong = MediaPlayer.create(beginnerActivity.this, R.raw.beginner_new);
     }
 
     public void backtoBeginner(View view) {
@@ -51,8 +50,70 @@ public class beginnerActivity extends AppCompatActivity {
         //reaching the active imageButton (play)
         ImageButton imgButton = findViewById(R.id.imageButton);
 
+        imgButton.setImageResource(android.R.drawable.ic_media_play);
+        //deactivate the button after first play - for later
+
+        //What should happen when the song is over for the first time? -> all comes here
+        beginnerSong.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+
+                //adding the start game button after song finishes for the first time
+
+                Button start_button = findViewById(R.id.startGame);
+                start_button.setVisibility(View.VISIBLE);
+
+                //changing the play button to replay after song finishes
+                ImageButton imgButton = findViewById(R.id.imageButton);
+                imgButton.setImageResource(R.drawable.replay);
+
+            }
+        });
+
+    }
+
+    public void playAndMatch(View view) {
+
+        //reaching the start game button to make hide it when the user starts the gane
+        Button start_button = findViewById(R.id.startGame);
+        start_button.setVisibility(View.GONE);
+
+
+        //adding the green user_input button after song finishes
+        Button userButton = findViewById(R.id.user_button);
+        userButton.setVisibility(View.VISIBLE);
+
+
+        // play the song
+        beginnerSong.start();
+
+
+        //reaching the active imageButton (play)
+        ImageButton imgButton = findViewById(R.id.imageButton);
+
         //changing the play button to pause while playing
-        imgButton.setImageResource(android.R.drawable.ic_media_pause);
+        imgButton.setImageResource(android.R.drawable.ic_media_play);
+
+        //we moved this piece of code up because the click events on the user button are not happenning on song completion (but during the song)
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // update user beat list
+                if (user_beat.size() == 0) {
+                    user_beat.add((long) 0);
+                } else {
+                    long click_time = System.currentTimeMillis();
+                    user_beat.add(click_time - user_beat.get(0));   // TimeDistance to first click
+                    Log.d("TAg", (click_time + ""));
+                }
+
+                // draw new points
+
+
+            }
+        });
+
+
         //deactivate the button after first play - for later
 
         //try to dynamically put dots on the purple line
@@ -69,26 +130,13 @@ public class beginnerActivity extends AppCompatActivity {
                 // initialize the user beat list
                 // user_beat = new ArrayList<Long>();
 
-                //adding the green user_input button after song finishes
                 Button userButton = findViewById(R.id.user_button);
-                userButton.setVisibility(View.VISIBLE);
-                userButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // update user beat list
-                        if (user_beat.size() == 0) {
-                            user_beat.add((long) 0);
-                        } else {
-                            long click_time = System.currentTimeMillis();
-                            user_beat.add(click_time - user_beat.get(0));   // TimeDistance to first click
-                            Log.d("TAg", (click_time + ""));
-                        }
+                userButton.setVisibility(View.GONE);
 
-                        // draw new points
+                Button start_button = findViewById(R.id.startGame);
+                start_button.setText(getString(R.string.RESTART));
+                start_button.setVisibility(View.VISIBLE);
 
-
-                    }
-                });
 
                 //changing the play button to replay after song finishes
                 ImageButton imgButton = findViewById(R.id.imageButton);
