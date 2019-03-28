ab9d4d3c467fd4fd2f6722c5119299f458a61c37
OjaUlm <veronika.ansorge@uni-ulm.de>
2019-03-28 14:30:19 +0100

    small chane MainActitivy

--- app/src/main/java/com/example/diana/testrhythmer/MainActivity.java
+++ app/src/main/java/com/example/diana/testrhythmer/MainActivity.java
@@ -2,21 +2,16 @@ package com.example.diana.testrhythmer;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
 private EditText editText;
-
 boolean empty = false;
-
 private static final String SHARED_PREF_NAME="username";
 private static final String KEY_NAME="key_username";
 
@@ -24,7 +19,6 @@ private static final String KEY_NAME="key_username";
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editText= findViewById(R.id.editText);
     }
 
@@ -32,7 +26,7 @@ private static final String KEY_NAME="key_username";
         String name = editText.getText().toString().trim();
         empty = false;
 
-        if (name.isEmpty()){
+        if (name.isEmpty()){       // if no string was typed in, threw an error message
             empty = true;
             editText.setError("Please fill in your name!");
             editText.requestFocus();
@@ -50,14 +44,9 @@ private static final String KEY_NAME="key_username";
         // Do something in response to button the sends the user to logIn activity which is the activity where they choose their mode(beginner,advanced etc
 
         Intent intent = new Intent(this,loginActivity.class);
-       saveName();
+        saveName();
 
-
-        if (empty) {// if the name is empty is throws an error
-            editText.setError("Please fill in your name!");
-            editText.requestFocus();
-
-        } else {// otherwise it starts the activity
+        if (!empty) {// if a String is typed in, start next activity
             startActivity(intent);
         }
     }
--- app/src/main/java/com/example/diana/testrhythmer/loginActivity.java
+++ app/src/main/java/com/example/diana/testrhythmer/loginActivity.java
@@ -5,7 +5,6 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
